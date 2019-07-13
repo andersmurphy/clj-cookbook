dccfb2d1593c93ca2d3f52837a981991b2fdfea3
andersmurphy <andersmurphy@gmail.com>
2019-07-13 17:26:12 +0100

    Add hikari connection pooling example

---
+++ connection-pooling/hikari-cp-example/project.clj
@@ -0,0 +1,6 @@
+(defproject hikari-cp-example "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/java.jdbc "0.7.7"]
+                 [org.postgresql/postgresql "42.2.3"]
+                 [hikari-cp "2.7.1"]
+                 [ch.qos.logback/logback-classic "1.2.3"]])
